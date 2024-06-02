5642a20f233b635b8a2dba51b15a88abe968e3c7
Carlos Eduardo Niederauer Rodrigues <55125980+ceniederauer@users.noreply.github.com>
2024-06-02 20:39:43 -0300

    Remove @Table name from both Models because was unnecessary

--- src/main/java/com/RamenGo/api/models/Protein.java
+++ src/main/java/com/RamenGo/api/models/Protein.java
@@ -5,14 +5,12 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.hibernate.annotations.UuidGenerator;
 
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
-@Table(name = "proteins")
 public class Protein {
 
   /**
